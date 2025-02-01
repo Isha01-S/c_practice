66e0591f6503b02dd0f96b34c3d45a67cf365c39
Isha01-S <ishamca01@gmail.com>
2025-02-01 09:38:12 +0530

    added one more loop and used goto in it

--- inverted_pyramid.c
+++ inverted_pyramid.c
@@ -20,5 +20,35 @@ printf("\nloop 1\n\n");
 		}
 		printf("\n");
 	}
+
+
+//loop2
+printf("\nloop2\n");
+
+	for (i=1; i<=5; i++)
+	{
+		for (j=5; j>=i; j--)
+		{
+				if (i==4)
+				{
+				goto loop;
+				}
+			printf("%d",i);
+		}
+		printf("\n");
+	}
+loop:
+	for(i=2; i>=1; i--)
+	{
+		for (j=1; j<=i;j++)
+		{
+			printf("%d",i);
+		}
+		printf("\n");
+	}
+		goto end;
+end:
+	printf("\n");
+
 return 0;
 }
