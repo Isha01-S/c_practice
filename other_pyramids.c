53873fe386c15a9f95b2fbf5fa1168b56ef81e77
Isha01-S <ishamca01@gmail.com>
2025-02-28 21:46:01 +0530

    added few pyramids with space

--- other_pyramids.c
+++ other_pyramids.c
@@ -3,10 +3,11 @@
 #include <stdio.h>
 int main()
 {
-	int n,i,j;
+	int n,i,j,k;
 	printf("Enter n:");
 	scanf("%d",&n);
 	
+	printf("Pyramid 1\n");
 	for(i=1;i<=n;i++)//for number of lines
 	{
 		for(j=1;j<=n-i;j++)//for printing space
@@ -15,11 +16,134 @@ int main()
 		}
 		for(j=1;j<=i;j++)//for printing space
 		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+
+	printf("Pyramid 2\n");
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n-i;j++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=i;j++)
+		{
+			printf("%d ",j);
+		}
+		printf("\n");
+
+	}
+
+	printf("Pyramid 3\n");
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n-i;j++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=i;j++)
+		{
 			printf("*");
 		}
 		printf("\n");
 	}
 
+	printf("Pyramid 4\n");
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n-i;j++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=i;j++)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+
+	printf("Pyramid 5\n");
+
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n-i;j++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=2*i-1;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	printf("Pyramid 6\n");
+
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n-i;j++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=2*i-1;j++)
+		{
+			printf("%d",i);
+		}
+		printf("\n");
+	}
+
+	printf("Pyramid 7\n");
+
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n-i;j++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=2*i-1;j++)
+		{
+			printf("%d",j);
+		}
+		printf("\n");
+	}
+
+	printf("Pyramid 8\n");
+
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=2*(n-i);j++)
+		{
+			printf(" ");
+		}
+		for(j=1;j<=2*i-1;j++)
+		{
+			printf("* ");
+		}
+		printf("\n");
+	}
+
+	printf("Pyramid 9\n");
+
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n-i;j++)
+		{
+			printf(" ");
+		}
+		k=i;
+		for(j=1;j<=i;j++)
+		{
+			printf("%d",k++);
+		}
+		k=k-2;
+		for(j=1;j<=i-1;j++)
+		{
+			printf("%d",k--);
+		}
+		printf("\n");
+	}
 	return 0;
 }
 
