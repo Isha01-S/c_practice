07225d015019b96ece4d74a0d0cd00fbfb35ea39
Isha01-S <ishamca01@gmail.com>
2025-09-22 11:19:18 +0530

    added print the value and address of the elements of an array

---
+++ pointers/07_value_address_array.c
@@ -0,0 +1,12 @@
+/*Program to print the value and address of the elements of an array*/
+
+#include<stdio.h>
+int main(){
+    int arr[5]={5,10,15,20,25};
+    int i;
+    for(i=0;i<5;i++){
+        printf("value of arr[%d] = %d\n",i,arr[i]);
+        printf("address of arr[%d]  = %p\n",i,&arr[i]);
+    }
+    return 0;
+}
