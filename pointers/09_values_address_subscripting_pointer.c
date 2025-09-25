df3d4eb2d48f961a36cc13fbacb928398762cb11
Isha01-S <ishamca01@gmail.com>
2025-09-25 22:31:43 +0530

    printed values and address of array by subscripting the pointer

---
+++ pointers/09_values_address_subscripting_pointer.c
@@ -0,0 +1,13 @@
+/*Programt to print the value and address of array elements by subscripting a pointer variable*/
+#include<stdio.h>
+int main()
+{
+    int arr[5]={5,10,15,20,25};
+    int i, *p;
+    p=arr;
+    for(i=0;i<5;i++){
+        printf("address of arr[%d]= %p %p %p %p\n",i,&arr[i],arr+i,p+i,&p[i]);
+        printf("value of arr[%d]= %d %d %d %d\n",i,arr[i],*(arr+1),*(p+1),p[i]);
+    }
+    return 0;
+}
